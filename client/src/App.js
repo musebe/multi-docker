255206d31e291615c2096749f54bda171ed407cf
Eugene Musebe <eugenemusebe@Eugenes-MacBook-Pro.local>
2021-02-10 16:44:32 +0300

    Deployment commit

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+
+        <Link to="/">Home</Link> <br/> <br/>
+        <Link to="/otherpage">Other Page</Link>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          
+        </header> <br/><br/>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
